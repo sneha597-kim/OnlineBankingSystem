982a5c61e371ae3d14a14a54f8a48a2918cd7ed9
Sneha latha <snehalathamandhadi@gmail.com>
2025-11-07 18:03:49 +0530

    Update app.py

--- auth-service/app.py
+++ auth-service/app.py
@@ -73,5 +73,6 @@ def logout():
     flash("Logged out successfully.", "success")
     return redirect(url_for("login"))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
